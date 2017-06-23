766165bb8e31cfa93e71d4caa082e759b17ee105
user <alphonsosensleyii@this-macbook.local>
2017-06-23 15:03:04 -0700

    Concentrating on getting familiar with logic and @IBActions and @IBOutlets.

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -2,17 +2,27 @@
 //  ViewController.swift
 //  FunFacts
 //
-//  Created by user on 4/27/17.
+//  Created by user on 6/23/17.
 //  Copyright © 2017 Alphonso. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+var funFactArray = ["Fact1", "Fact2", "Fact3", "Fact4", "Fact5"]
+
+
+class ViewController: UIViewController {
+    @IBOutlet weak var funFactLabel: UILabel!
+    var labelValue: String?
+    var buttonPresses: Int = 0
+    
+    
+    
+       override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        funFactLabel.text = "Another Interesting Fact"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showFact() {
+        //Label text will equal the funFactArray position using the incremented variable value
+        funFactLabel.text = funFactArray[buttonPresses]
+        buttonPresses += 1
+        //The following line insures no out of bounds
+        if (buttonPresses == funFactArray.count) {
+            buttonPresses = 0
+        }
 
+        //added a switch statement to increment through background colors on button presses
+        switch buttonPresses {
+        case 1:
+           self.view.backgroundColor = UIColor.gray;
+        case 2:
+           self.view.backgroundColor = UIColor.blue
+        case 3:
+            self.view.backgroundColor = UIColor.green
+        case 4:
+            self.view.backgroundColor = UIColor.yellow
+        default:
+            self.view.backgroundColor = UIColor.white
+        }
+        
+    }
 }
+
+
+
+
+
+
+
+
+
+
 
